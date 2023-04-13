5befc46f62965c6ead63dee3226e5da2cafa57b1
pedroabm <pamaltez@hotmail.com>
2023-04-13 14:27:36 -0300

    Merge remote-tracking branch 'origin/main'

--- Veiculo-Abstract/src/Carro.java
+++ Veiculo-Abstract/src/Carro.java
@@ -1,6 +1,6 @@
 public class Carro extends Veiculo {
     @Override
     public void acelerar() {
-        System.out.println("car is running!!"); // Change have been made here again!
+        System.out.println("car is running!!!"); // Change have been made here again!
     }
 }

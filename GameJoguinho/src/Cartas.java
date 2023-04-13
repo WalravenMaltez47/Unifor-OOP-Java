01798c303df51898be9337d54d97d8c7427c6412
pedroabm <pamaltez@hotmail.com>
2023-04-13 13:17:01 -0300

    Merge remote-tracking branch 'origin/main'

--- GameJoguinho/src/Cartas.java
+++ GameJoguinho/src/Cartas.java
@@ -1,6 +1,6 @@
 public class Cartas extends Jogo {
     @Override
     public void jogar() {
-        System.out.println("Estou jogando Uno");
+        System.out.println("Estou jogando Uno!");
     }
 }

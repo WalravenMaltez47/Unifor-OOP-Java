5d541e422fbc4ed55e089fe9d698973a9f45ea96
pedroabm <pamaltez@hotmail.com>
2023-04-15 12:36:46 -0300

    Merge remote-tracking branch 'origin/main'

--- Super Heros vs Alien battle on Earth edition/src/RandomMethod.java
+++ Super Heros vs Alien battle on Earth edition/src/RandomMethod.java
@@ -4,5 +4,15 @@ public class RandomMethod {
         Random random = new Random();
         return random.nextInt(6) + 1;
     }
+
+
+   /* // Uma outra forma de fazer caso queira escolher os valores de cada face do dado e seu tamanho.
+
+   int[] numeros = {1,2,3,4,5,6};
+    public int gerarNumeroAleatorio() {
+        Random random = new Random();
+        int comprimento = random.nextInt(numeros.length);
+        return numeros[comprimento];
+    } */
 }
 

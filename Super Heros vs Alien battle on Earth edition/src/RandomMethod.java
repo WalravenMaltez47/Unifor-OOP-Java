bc0a1189fc2061c5ceb8652f64d41a56efeb145d
AugustoMaltez <pamaltez47@protonmail.com>
2023-05-05 14:52:50 -0300

    teste0

--- Super Heros vs Alien battle on Earth edition/src/RandomMethod.java
+++ Super Heros vs Alien battle on Earth edition/src/RandomMethod.java
@@ -7,16 +7,3 @@ public class RandomMethod {
     static public RandomMethod random = new RandomMethod();
     static public int dado;
 }
-
-
-/*
-   Uma outra forma de fazer caso queira
-   escolher os valores de cada face do
-    dado e seu tamanho.
-
-   int[] numeros = {1,2,3,4,5,6};
-    public int gerarNumeroAleatorio() {
-        Random random = new Random();
-        int comprimento = random.nextInt(numeros.length);
-        return numeros[comprimento];
-    } */

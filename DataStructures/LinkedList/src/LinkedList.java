cc6f7924cb10a5ebd7fcd96e20e6c9f45d52fb5f
Pedro Augusto Brito Maltez <pamaltez@hotmail.com>
2024-03-09 21:12:26 -0300

    coia=A

--- DataStructures/LinkedList/src/LinkedList.java
+++ DataStructures/LinkedList/src/LinkedList.java
@@ -1,61 +1,131 @@
-public class LinkedList<T> implements Interface<T> {
-    private No<T> inicioNo;
-    private No<T> ultimoNo;
-    private int size = 0;
+import java.util.NoSuchElementException;
+
+public class LinkedList<T> {
+    private static class No<T> {
+        public T valor;
+        public No<T> proximo;
+    
+        public No(T valor) {
+            this.valor = valor;
+            this.proximo = null;
+        }
+    }
+
+    private No<T> primeiro;
+    private No<T> ultimo;
+    private int tamanho;
 
     LinkedList() {
-        
+        this.primeiro = null;
+        this.ultimo = null;
+        this.tamanho = 0;
     }
 
-    @Override
-    public void add(T elemento) {
-        No<T> celula = new No<T>(elemento);
-
-        if (this.size == 0) {
-            this.inicioNo = celula;
+    @SuppressWarnings({ "unchecked", "rawtypes" })
+    public void insereInicio(int valor) {
+        No<T> novoNodo = new No(valor);
+        if (primeiro == null) {
+            primeiro = novoNodo;
+            ultimo = novoNodo;
+        } else {
+            novoNodo.proximo = primeiro;
+            primeiro = novoNodo;
         }
-
-        else this.ultimoNo.setProximo(celula);
-
-        this.inicioNo = celula;
-        this.ultimoNo = celula;
-        this.size++;
+        tamanho++;
     }
 
-    @Override
-    public int getSize() {
-        return this.size;
-    }
-
-    @Override
-    public void clean() {
-        for(No<T> atual = this.inicioNo; atual != null;) {
-            No<T> proximoNo = atual.getProximo();
-            atual.setElemento(null);
-            atual.setProximo(null);
+    public void removeInicio() {
+        if (primeiro == null) {
+            throw new NoSuchElementException("A lista está vazia");
+        } else {
+            primeiro = primeiro.proximo;
+            tamanho--;
+            if (tamanho == 0) {
+                ultimo = null;
+            }
         }
-        this.inicioNo = null;
-        this.ultimoNo = null;
-        this.size = 0;
-    }
-
-    @Override
-    public String toString() {
-        if (this.size == 0) {
-            return "[]";
-        }
-        
-        StringBuilder builder = new StringBuilder();
-        
-        No<T> atual = this.inicioNo;
-        builder.append(atual.getElemento()).append(" , ");
-    
-        while(atual.getProximo() != null) {
-        atual = atual.getProximo();
-        builder.append(atual.getElemento()).append(" , ");
-    }
-
-    return builder.toString();
     }
     
+
+    public void insereFinal(int valor) {
+        @SuppressWarnings({ "rawtypes", "unchecked" })
+        No<T> novoNodo = new No(valor);
+        if (ultimo == null) {
+            primeiro = novoNodo;
+            ultimo = novoNodo;
+        } else {
+            ultimo.proximo = novoNodo;
+            ultimo = novoNodo;
+        }
+        tamanho++;
+    }
+
+    public void removeFinal() {
+        if (ultimo == null) {
+            throw new NoSuchElementException("A lista está vazia");
+        } else {
+            if (primeiro == ultimo) {
+                primeiro = null;
+                ultimo = null;
+            } else {
+                No atual = primeiro;
+                while (atual.proximo != ultimo) {
+                    atual = atual.proximo;
+                }
+                atual.proximo = null;
+                ultimo = atual;
+            }
+            tamanho--;
+        }
+    }
+
+    public void inserePosicao(int valor, int posicao) {
+        if (posicao < 0 || posicao > tamanho) {
+            throw new IndexOutOfBoundsException("Posição inválida");
+        }
+
+        if (posicao == 0) {
+            insereInicio(valor);
+        } else if (posicao == tamanho) {
+            insereFinal(valor);
+        } else {
+            No<T> novoNodo = new No(valor);
+            No<T> atual = primeiro;
+            for (int i = 0; i < posicao - 1; i++) {
+                atual = atual.proximo;
+            }
+            novoNodo.proximo = atual.proximo;
+            atual.proximo = novoNodo;
+            tamanho++;
+        }
+    }
+
+    public void removePosicao(int posicao) {
+        if (posicao < 0 || posicao >= tamanho) {
+            throw new IndexOutOfBoundsException("Posição inválida");
+        }
+
+        if (posicao == 0) {
+            removeInicio();
+        } else if (posicao == tamanho - 1) {
+            removeFinal();
+        } else {
+            No<T> atual = primeiro;
+            for (int i = 0; i < posicao - 1; i++) {
+                atual = atual.proximo;
+            }
+            atual.proximo = atual.proximo.proximo;
+            tamanho--;
+        }
+    }
+
+    public void imprimirLista() {
+        No<T> atual = primeiro;
+        System.out.print("Lista: ");
+        while (atual != null) {
+            System.out.print(atual.valor + " ");
+            atual = atual.proximo;
+        }
+        System.out.println();
+    }
 }
--- DataStructures/LinkedList/src/Main.java
+++ DataStructures/LinkedList/src/Main.java
@@ -1,18 +1,26 @@
 public class Main {
-public static void main(String[] args) {
-    LinkedList<Integer> lista = new LinkedList<>();
+    public static void main(String[] args) {
+    LinkedList<Integer> lista = new LinkedList<>(); 
+    lista.insereInicio(3);
+    lista.insereInicio(2);
+    lista.insereInicio(1);
+    lista.imprimirLista(); // Saída: 1 2 3
 
-    lista.add(1);
-    System.out.println(lista);
+    lista.insereFinal(4);
+    lista.insereFinal(5);
+    lista.imprimirLista(); // Saída: 1 2 3 4 5
 
-    lista.add(2);
-    System.out.println(lista);
+    lista.inserePosicao(10, 2);
+    lista.imprimirLista(); // Saída: 1 2 10 3 4 5
 
-    lista.add(3);
-    System.out.println(lista);
+    lista.removePosicao(3);
+    lista.imprimirLista(); // Saída: 1 2 10 4 5
 
-    lista.clean();
-    System.out.println(lista);
+    lista.removeInicio();
+    lista.imprimirLista(); // Saída: 2 10 4 5
 
+    lista.removeFinal();
+    lista.imprimirLista(); // Saída: 2 10 4
+    
 }
 }

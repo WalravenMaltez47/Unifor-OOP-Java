39a37d591c7ea071b0a268b69868da485fbd8a12
Pedro Augusto Brito Maltez <pamaltez@hotmail.com>
2024-04-26 20:05:45 -0300

    sd

--- DataStructures/Fila/FilaDeRecreio.java
+++ DataStructures/Fila/FilaDeRecreio.java
@@ -1,42 +1,86 @@
 import java.util.Scanner;
-
-public class FilaDeRecreio {
+class Main {
     public static void main(String[] args) {
         try (Scanner sc = new Scanner(System.in)) {
-            int N = sc.nextInt(); // Lê o número de casos de teste
-            
-            for (int caso = 0; caso < N; caso++) {
-                int M = sc.nextInt(); // Lê o número de alunos para esse caso
-                int[][] alunos = new int[M][2];
-                for (int i = 0; i < M; i++) {
-                    alunos[i][0] = sc.nextInt(); // Nota do aluno
-                    alunos[i][1] = i; // Índice original
+            int N = sc.nextInt(); 
+            for (int i = 0; i < N; i++) {
+                int M = sc.nextInt(); 
+                FilaDeRecreio filaDeRecreio = new FilaDeRecreio(M); 
+                for (int j = 0; j < M; j++) {
+                    int nota = sc.nextInt();
+                    filaDeRecreio.add(nota);
                 }
-                
-                // Implementação do Bubble Sort para ordenar de forma decrescente por nota
-                boolean swapped;
-                do {
-                    swapped = false;
-                    for (int i = 1; i < M; i++) {
-                        if (alunos[i - 1][0] < alunos[i][0]) { // Compara e troca se necessário
-                            int[] temp = alunos[i - 1];
-                            alunos[i - 1] = alunos[i];
-                            alunos[i] = temp;
-                            swapped = true;
-                        }
-                    }
-                } while (swapped);
-                
-                int count = 0;
-                for (int i = 0; i < M; i++) {
-                    if (alunos[i][1] == i) {
-                        count++; // Conta quantos alunos mantiveram a mesma posição
-                    }
-                }
-                
-                System.out.println(count); // Imprime o resultado para o caso de teste atual
+                filaDeRecreio.print(M);
             }
-        } // Lê o número de casos de teste
+        }
     }
 }
 
+public class FilaDeRecreio {
+    Fila2D fila;
+
+    public FilaDeRecreio(int tamanho) {
+        this.fila = new Fila2D(tamanho);
+    }
+
+    public void add(int nota) {
+        if (fila == null) {
+            System.out.println("Erro: Fila não inicializada corretamente.");
+            return;
+        }
+        fila.insere(nota);
+    }
+
+    public void print(int M) {
+        if (fila == null) {
+            System.out.println("Erro: Fila não inicializada corretamente.");
+            return;
+        }
+        int count = 0;
+        fila.trocar();
+        for (int i = 0; i < M; i++) {
+            if (fila.getFilaColuna2(i) == i) {
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
+
+class Fila2D {
+    private int[][] fila;
+    private int size;
+
+    Fila2D(int tamanho) {
+        this.fila = new int[tamanho][2];
+        this.size = 0;
+    }
+
+    public void insere(int valor) {
+        fila[size][0] = valor;
+        fila[size][1] = size; 
+        size++;
+    }
+
+    public int[][] getFila() {
+        return fila;
+    }
+
+    public int getFilaColuna2(int pos) {
+        return fila[pos][1];
+    }
+
+    public void trocar() {
+       
+        for (int i = 0; i < size - 1; i++) {
+            for (int j = 0; j < size - i - 1; j++) {
+
+                if (fila[j][0] < fila[j + 1][0]) {
+                    int[] temp = fila[j];
+                    fila[j] = fila[j + 1];
+                    fila[j + 1] = temp;
+                }
+            }
+        }
+    }
+}
